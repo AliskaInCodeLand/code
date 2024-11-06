050e3bd1a5779ff36aa1aa6998c1776962f34398
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-11-06 15:50:27 +0300

    add commit text

--- src/practic/main.js
+++ src/practic/main.js
@@ -3,3 +3,9 @@
 const arr = [1, 2, 3, 4, 5];
 const resultSumElementsArray = arr.reduce((a, b) => a + b);
 console.log(resultSumElementsArray);
+
+//Комбинация клавиш для консоли коммитов
+//Сtrl + K
+//Далее пишем текстовой комментарий
+//Нажимаем коммит и push
+//И в окне нажимаем push
