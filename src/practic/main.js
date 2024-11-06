b637565c3cd018bac135a2a78938f9b9a307b4cc
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-11-06 15:44:50 +0300

    function

--- src/practic/main.js
+++ src/practic/main.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+
+const arr = [1, 2, 3, 4, 5];
+const resultSumElementsArray = arr.reduce((a, b) => a + b);
+console.log(resultSumElementsArray);
