fda5709a2f9a82d5ed625b8347c15df5f77808db
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-11-20 23:18:26 +0300

    function Moving Zeros To The End other solution

--- src/practic/main.js
+++ src/practic/main.js
@@ -2036,13 +2036,25 @@
    *
    * moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]*/
 
-  function moveZeros(arr) {
-    let newArr = arr.filter((item) => item !== 0);
-    while (newArr.length < arr.length) {
-      newArr.push(0);
-    }
-    return newArr;
-  }
+  // function moveZeros(arr) {
+  //   let newArr = arr.filter((item) => item !== 0);
+  //   while (newArr.length < arr.length) {
+  //     newArr.push(0);
+  //   }
+  //   return newArr;
+  // }
+
+  var moveZeros = function (arr) {
+    return arr
+      .filter(function (x) {
+        return x !== 0;
+      })
+      .concat(
+        arr.filter(function (x) {
+          return x === 0;
+        }),
+      );
+  };
 
   console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
 }
