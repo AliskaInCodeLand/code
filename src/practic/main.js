9c6b89b84a0e17b8df011a47bfcb4574d342e8ab
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-11-23 22:56:21 +0300

    function Object.values, task#2

--- src/practic/main.js
+++ src/practic/main.js
@@ -2297,33 +2297,58 @@
 // }
 
 //Решение задач Object.keys, values, entries 23/11
-{
-  //Сумма свойств объекта
-  /**
-   * Есть объект salaries с произвольным количеством свойств,
-   * содержащих заработные платы.
-   *
-   * Напишите функцию sumSalaries(salaries), которая возвращает
-   * сумму всех зарплат с помощью метода Object.values и цикла for..of.
-   *
-   * Если объект salaries пуст, то результат должен быть 0.
-   *
-   * Например:
-   * let salaries = {
-   *   "John": 100,
-   *   "Pete": 300,
-   *   "Mary": 250
-   * };*/
+// {
+//   //Сумма свойств объекта
+//   /**
+//    * Есть объект salaries с произвольным количеством свойств,
+//    * содержащих заработные платы.
+//    *
+//    * Напишите функцию sumSalaries(salaries), которая возвращает
+//    * сумму всех зарплат с помощью метода Object.values и цикла for..of.
+//    *
+//    * Если объект salaries пуст, то результат должен быть 0.
+//    *
+//    * Например:
+//    * let salaries = {
+//    *   "John": 100,
+//    *   "Pete": 300,
+//    *   "Mary": 250
+//    * };*/
+//
+//   let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+//   };
+//
+//   // function sumSalaries(salaries) {
+//   //   return Object.values(salaries).reduce((acc, cur) => acc + cur, 0);
+//   // }
+//
+//   console.log(sumSalaries(salaries));
+// }
 
-  let salaries = {
-    John: 100,
-    Pete: 300,
-    Mary: 250,
+{
+  //Подсчёт количества свойств объекта
+  /**
+   * Напишите функцию count(obj),
+   * которая возвращает количество свойств объекта:
+   * let user = {
+   *   name: 'John',
+   *   age: 30
+   * };
+   *
+   * alert( count(user) ); // 2
+   * */
+
+  let user = {
+    name: "John",
+    age: 30,
   };
 
-  function sumSalaries(salaries) {
-    return Object.values(salaries).reduce((acc, cur) => acc + cur, 0);
+  function count(obj) {
+    return Object.keys(obj).length;
   }
 
-  console.log(sumSalaries(salaries));
+  console.log(count(user));
 }
