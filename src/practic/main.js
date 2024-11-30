3c15925cca24e29fde6320c415f6345952f0cca8
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-11-30 21:13:06 +0300

    function same good_solution#2

--- src/practic/main.js
+++ src/practic/main.js
@@ -2991,7 +2991,7 @@
 //Решение задач CodeWars 30/11
 {
   //Are they the "same"?
-  function comp(a, b) {
+  function comp(a1, a2) {
     //проверка на массив,а не null
     if (a1 == null || a2 == null) return false;
     for (var i = 0; i < a2.length; i++) {
