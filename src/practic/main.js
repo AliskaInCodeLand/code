d107a465cc6e831f5c31f86f5c9333ffa3745c45
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-01 23:40:19 +0300

    function adjacentElementsProduct map and slice

--- src/practic/main.js
+++ src/practic/main.js
@@ -3072,14 +3072,19 @@
   // }
 
   //other solution
-  {
-    function adjacentElementsProduct(array) {
-      let newArr = [];
-      for (i = 0; i < array.length - 1; i++) {
-        newArr.push(array[i] * array[i + 1]);
-      }
-      return Math.max(...newArr);
-    }
+  // {
+  //   function adjacentElementsProduct(array) {
+  //     let newArr = [];
+  //     for (i = 0; i < array.length - 1; i++) {
+  //       newArr.push(array[i] * array[i + 1]);
+  //     }
+  //     return Math.max(...newArr);
+  //   }
+  // }
+
+  //other solution
+  function adjacentElementsProduct(a) {
+    return Math.max(...a.map((x, i) => x * a[i + 1]).slice(0, -1));
   }
 
   console.log(adjacentElementsProduct([1, 2, 3]));
