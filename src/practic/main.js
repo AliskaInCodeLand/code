e6326ef31eafccda5cdcd339fd3abc64da77c027
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-03 23:32:55 +0300

    function simple frequency sort

--- src/practic/main.js
+++ src/practic/main.js
@@ -3129,17 +3129,24 @@
    * solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
    * -- We sort by highest frequency to lowest frequency.
    * -- If two elements have same frequency, we sort by increasing value.*/
-  function solve(data) {
-    const freq = data.reduce((r, e) => {
-      if (!r[e]) {
-        r[e] = 1;
-      } else r[e]++;
-      return r;
-    }, {});
+  // function solve(data) {
+  //   const freq = data.reduce((r, e) => {
+  //     if (!r[e]) {
+  //       r[e] = 1;
+  //     } else r[e]++;
+  //     return r;
+  //   }, {});
+  //
+  //   return [...data].sort((a, b) => {
+  //     return freq[b] - freq[a] || a - b;
+  //   });
+  // }
 
-    return [...data].sort((a, b) => {
-      return freq[b] - freq[a] || a - b;
-    });
+  //other solution
+  function solve(arr) {
+    var r = {};
+    for (var n of arr) r[n] = r[n] + 1 || 1;
+    return arr.slice().sort((a, b) => r[b] - r[a] || a - b);
   }
 
   console.log(solve([1, 2, 3, 0, 5, 0, 1, 6, 8, 8, 6, 9, 1]));
