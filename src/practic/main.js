e9cfab7833c5178b812339b4de2430eb975a42bd
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-07 18:44:05 +0300

    function polindrome solution with for

--- src/practic/main.js
+++ src/practic/main.js
@@ -3415,8 +3415,18 @@
   //     .join("");
   //   console.log(newWord === word);
 
+  // function polindrome(str) {
+  //   str = str.toLowerCase().replace(/\s/g, "");
+  //   console.log(str === str.split("").reverse().join(""));
+  // }
+
   function polindrome(str) {
-    str = str.toLowerCase().replace(/\s/g, "");
-    console.log(str === str.split("").reverse().join(""));
+    const len = Math.floor(str.length / 2);
+    for (let i = 0; i < len; i++) {
+      if (str[i] !== str[str.length - i - 1]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
