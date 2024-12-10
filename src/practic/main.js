54f65c3d6b92c29fa84d6660160a97604c53003a
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-10 20:52:10 +0300

    function my version maze

--- src/practic/main.js
+++ src/practic/main.js
@@ -3704,30 +3704,69 @@
     [1, 1, 1, 1, 1, 0],
   ];
 
+  // function checkPath(start, end) {
+  //   maze[start.y][start.x] = 5;
+  //   let siblings = getValidSib(start);
+  //   if (siblings.length > 0) {
+  //     for (let i = 0; i < siblings.length; i++) {
+  //       let current = siblings[i];
+  //
+  //       let isSolved = current.x === end.x && current.y === end.y;
+  //       let notVisited = maze[current.y][current.x] !== 5;
+  //
+  //       if (isSolved || (notVisited && checkPath(current, end))) {
+  //         return true;
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // }
+  //
+  // function getValidSib(cord) {
+  //   let { x, y } = cord;
+  //
+  //   let cords = [];
+  //   // debugger;
+  //
+  //   if (maze[y - 1] !== undefined) {
+  //     cords.push({ x: x, y: y - 1, val: maze[y - 1][x] });
+  //   }
+  //   if (maze[y + 1] !== undefined) {
+  //     cords.push({ x: x, y: y + 1, val: maze[y + 1][x] });
+  //   }
+  //
+  //   if (maze[y][x - 1] !== undefined) {
+  //     cords.push({ x: x - 1, y: y, val: maze[y][x - 1] });
+  //   }
+  //   if (maze[y][x + 1] !== undefined) {
+  //     cords.push({ x: x + 1, y: y, val: maze[y][x + 1] });
+  //   }
+  //
+  //   return cords.filter((el) => el.val === 0);
+  // }
+
   function checkPath(start, end) {
-    debugger;
-    maze[start.y][start.x] = 5;
-    let siblings = getValidSib(start);
+    maze[start.y][start.x] = 2;
+    let siblings = getValuesCord(start);
+
     if (siblings.length > 0) {
       for (let i = 0; i < siblings.length; i++) {
         let current = siblings[i];
 
         let isSolved = current.x === end.x && current.y === end.y;
-        let notVisited = maze[current.y][current.x] !== 5;
+        let isVisited = maze[current.x][current.y] != 5;
 
-        if (isSolved || (notVisited && checkPath(current, end))) {
+        if (isSolved || (isVisited && checkPath(current, end))) {
           return true;
         }
       }
+      return false;
     }
-    return false;
   }
 
-  function getValidSib(cord) {
+  function getValuesCord(cord) {
     let { x, y } = cord;
-
     let cords = [];
-    // debugger;
 
     if (maze[y - 1] !== undefined) {
       cords.push({ x: x, y: y - 1, val: maze[y - 1][x] });
@@ -3742,7 +3781,6 @@
     if (maze[y][x + 1] !== undefined) {
       cords.push({ x: x + 1, y: y, val: maze[y][x + 1] });
     }
-
     return cords.filter((el) => el.val === 0);
   }
 
