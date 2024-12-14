080d7f3347a41015f045a70302c807e3d49a1e2f
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-14 22:55:35 +0300

    function distance to closest person optimization

--- src/practic/main2.js
+++ src/practic/main2.js
@@ -118,29 +118,62 @@
 
 //Решение задач 14/12
 {
-  const input1 = [1, 0, 0, 0, 1, 0, 1]; //2
+  const input1 = [0, 0, 0, 1, 0, 0, 0, 1]; //2
   const input2 = [1, 0, 0, 0]; //3
+
+  // const maxDistToClosest = function (seats) {
+  //   let max = 0;
+  //   let sumNone = 0;
+  //   let num = 0;
+  //   for (let i = 0; i < seats.length; i++) {
+  //     debugger;
+  //     if (seats[i] === 1 && num === 0) {
+  //       num = seats[i];
+  //       continue;
+  //     }
+  //     if (seats[i] === 0) {
+  //       sumNone++;
+  //       if (sumNone > max) {
+  //         max = sumNone;
+  //       }
+  //     }
+  //     if (seats[i] === 1 && num !== 0) {
+  //       num = 0;
+  //       sumNone = 0;
+  //       max--;
+  //     }
+  //   }
+  //   return max;
+  // };
 
   const maxDistToClosest = function (seats) {
     let max = 0;
-    let sumNone = 0;
-    let num = 0;
-    for (let i = 0; i < seats.length; i++) {
-      debugger;
-      if (seats[i] === 1 && num === 0) {
-        num = seats[i];
-        continue;
+    let count = 0;
+    let i = 0;
+
+    if (seats[i] === 0) {
+      while (seats[i] === 0) {
+        i++;
+        count++;
       }
-      if (seats[i] === 0) {
-        sumNone++;
-        if (sumNone > max) {
-          max = sumNone;
-        }
+
+      max = count;
+      count = 0;
+    }
+
+    for (; i < seats.length; i++) {
+      let current = seats[i];
+      if (i === seats.length - 1 && current === 0) {
+        count++;
+        max = Math.max(max, count);
+        break;
       }
-      if (seats[i] === 1 && num !== 0) {
-        num = 0;
-        sumNone = 0;
-        max--;
+
+      if (current === 1) {
+        count = 0;
+      } else {
+        count++;
+        max = Math.max(max, Math.ceil(count / 2));
       }
     }
     return max;
