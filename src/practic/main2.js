fb279e69b33b02cbb662c53573d16b06a708d872
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-14 23:58:51 +0300

    function matrix

--- src/practic/main2.js
+++ src/practic/main2.js
@@ -117,68 +117,125 @@
 // }
 
 //Решение задач 14/12
+// {
+//   const input1 = [0, 0, 0, 1, 0, 0, 0, 1]; //2
+//   const input2 = [1, 0, 0, 0]; //3
+//
+//   // const maxDistToClosest = function (seats) {
+//   //   let max = 0;
+//   //   let sumNone = 0;
+//   //   let num = 0;
+//   //   for (let i = 0; i < seats.length; i++) {
+//   //     debugger;
+//   //     if (seats[i] === 1 && num === 0) {
+//   //       num = seats[i];
+//   //       continue;
+//   //     }
+//   //     if (seats[i] === 0) {
+//   //       sumNone++;
+//   //       if (sumNone > max) {
+//   //         max = sumNone;
+//   //       }
+//   //     }
+//   //     if (seats[i] === 1 && num !== 0) {
+//   //       num = 0;
+//   //       sumNone = 0;
+//   //       max--;
+//   //     }
+//   //   }
+//   //   return max;
+//   // };
+//
+//   // const maxDistToClosest = function (seats) {
+//   //   let max = 0;
+//   //   let count = 0;
+//   //   let i = 0;
+//   //
+//   //   if (seats[i] === 0) {
+//   //     while (seats[i] === 0) {
+//   //       i++;
+//   //       count++;
+//   //     }
+//   //
+//   //     max = count;
+//   //     count = 0;
+//   //   }
+//   //
+//   //   for (; i < seats.length; i++) {
+//   //     let current = seats[i];
+//   //     if (i === seats.length - 1 && current === 0) {
+//   //       count++;
+//   //       max = Math.max(max, count);
+//   //       break;
+//   //     }
+//   //
+//   //     if (current === 1) {
+//   //       count = 0;
+//   //     } else {
+//   //       count++;
+//   //       max = Math.max(max, Math.ceil(count / 2));
+//   //     }
+//   //   }
+//   //   return max;
+//   // };
+//   //
+//   // console.log(maxDistToClosest(input1));
+//   // console.log(maxDistToClosest(input2));
+// }
+
+import { i } from "vite/dist/node/types.d-aGj9QkWt.js";
+
 {
-  const input1 = [0, 0, 0, 1, 0, 0, 0, 1]; //2
-  const input2 = [1, 0, 0, 0]; //3
+  const matrix1 = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
 
-  // const maxDistToClosest = function (seats) {
-  //   let max = 0;
-  //   let sumNone = 0;
-  //   let num = 0;
-  //   for (let i = 0; i < seats.length; i++) {
-  //     debugger;
-  //     if (seats[i] === 1 && num === 0) {
-  //       num = seats[i];
-  //       continue;
-  //     }
-  //     if (seats[i] === 0) {
-  //       sumNone++;
-  //       if (sumNone > max) {
-  //         max = sumNone;
-  //       }
-  //     }
-  //     if (seats[i] === 1 && num !== 0) {
-  //       num = 0;
-  //       sumNone = 0;
-  //       max--;
-  //     }
-  //   }
-  //   return max;
-  // };
+  const matrix2 = [
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5],
+  ];
 
-  const maxDistToClosest = function (seats) {
-    let max = 0;
-    let count = 0;
-    let i = 0;
+  function matrixWithZero(matrix) {
+    let ROWS = matrix.length;
+    let COLS = matrix[0].length;
+    let isCol = false;
 
-    if (seats[i] === 0) {
-      while (seats[i] === 0) {
-        i++;
-        count++;
+    for (let i = 0; i < ROWS; i++) {
+      if (matrix[i][0] === 0) {
+        isCol = true;
       }
-
-      max = count;
-      count = 0;
-    }
-
-    for (; i < seats.length; i++) {
-      let current = seats[i];
-      if (i === seats.length - 1 && current === 0) {
-        count++;
-        max = Math.max(max, count);
-        break;
-      }
-
-      if (current === 1) {
-        count = 0;
-      } else {
-        count++;
-        max = Math.max(max, Math.ceil(count / 2));
+      for (let j = 0; j < COLS; j++) {
+        if (matrix[i][j] === 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
       }
     }
-    return max;
-  };
+    for (let i = 1; i < ROWS; i++) {
+      for (let j = 1; j < COLS; j++) {
+        if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
 
-  console.log(maxDistToClosest(input1));
-  console.log(maxDistToClosest(input2));
+    if (matrix[0][0] === 0) {
+      for (let j = 0; j < ROWS; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+
+    if (isCol) {
+      for (let i = 0; i < COLS; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+    return matrix;
+  }
+
+  console.log(matrixWithZero(matrix1)); // [[1,0,1], [0,0,0], [1,0,1]]
+  console.log(matrixWithZero(matrix2)); // [[0,0,0,0], [0, 4, 5, 0], [0, 3, 1, 0]]
 }
