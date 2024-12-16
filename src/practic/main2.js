a7f9305edd2ba62a3400be882f1490601819ac57
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-16 22:14:55 +0300

    function video repeat sum of three

--- src/practic/main2.js
+++ src/practic/main2.js
@@ -3,23 +3,86 @@
 {
   const array = [-1, 0, 1, 2, -1, -4];
 
-  const threeSum = function (nums, target = 0) {
-    nums.sort((a, b) => a - b);
-    const res = [];
+  // const threeSum = function (nums, target = 0) {
+  //   const res = [];
+  //
+  //   if (nums.length < 3) return res;
+  //
+  //   nums = nums.sort((a, b) => a - b);
+  //
+  //   for (let i = 1; i < nums.length - 2; i++) {
+  //     if (nums[i] > target) {
+  //       break;
+  //     }
+  //
+  //     if (i > 0 && nums[i] === nums[i - 1]) {
+  //       continue;
+  //     }
+  //
+  //     let j = i + 1;
+  //     let k = nums.length - 1;
+  //     while (j < k) {
+  //       let sum = nums[i] + nums[j] + nums[k];
+  //       if (sum === target) {
+  //         res.push([nums[i], nums[j], nums[k]]);
+  //
+  //         while (nums[j] === nums[j + 1]) j++;
+  //         while (nums[j] === nums[j + 1]) k++;
+  //
+  //         j++;
+  //         k--;
+  //         continue;
+  //       }
+  //       if (sum < target) {
+  //         j++;
+  //         continue;
+  //       }
+  //       if (sum > target) {
+  //         k--;
+  //         continue;
+  //       }
+  //     }
+  //   }
+  //   return res;
+  // };
 
-    for (let i = 1; i < nums.length; i++) {
-      let firstNum = nums[i];
-      for (let j = i + 1; j < nums.length; j++) {
-        let secondNum = nums[j];
-        if (nums.includes(target - (firstNum + secondNum))) {
-          let threeIndex = nums.indexOf(target - (firstNum + secondNum));
-          if (threeIndex !== i && threeIndex !== j) {
-            let threeNum = nums[threeIndex];
-            res.push([firstNum, secondNum, threeNum]);
-          }
+  const threeSum = function (nums, target = 0) {
+    let res = [];
+
+    nums = nums.sort((a, b) => a - b);
+
+    if (nums.length < 3) return res;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+      let j = i + 1;
+      let k = nums.length - 1;
+      while (j < k) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if (sum === target) {
+          res.push([nums[i], nums[j], nums[k]]);
+
+          while (nums[j] === nums[j + 1]) j++;
+          while (nums[k] === nums[k - 1]) k--;
+
+          j++;
+          k--;
+          continue;
+        }
+
+        if (sum < target) {
+          j++;
+          continue;
+        }
+
+        if (sum > target) {
+          k--;
+          continue;
         }
       }
     }
+
     return res;
   };
 
