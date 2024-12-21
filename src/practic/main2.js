eb4c878a793e6b9fefeb11754387fd1ac29f0dfc
Aliska Alekseeva <aliska05122002@yandex.ru>
2024-12-21 21:15:02 +0300

    function object three

--- src/practic/main2.js
+++ src/practic/main2.js
@@ -248,10 +248,3 @@ let prices = {
   orange: 2,
   meat: 4,
 };
-
-let doublePrices = Object.fromEntries(
-  // преобразовать в массив, затем map, затем fromEntries (обратно в объект)
-  Object.entries(prices).map(([key, value]) => [key, value * 2]),
-);
-
-alert(doublePrices.meat); // 8
